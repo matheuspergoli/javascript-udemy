d53d8e4e97f19b298bac31a4920de6f73cd9adfb
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-02-27 12:37:55 -0300

    aula15

---
+++ aula15/script.js
@@ -0,0 +1,25 @@
+let num1 = 25
+
+// Essa é uma forma de pegar a raiz quadrada de qualquer número
+console.log(num1 ** 0.5)
+
+// Arredondando o valor para baixo com o método Math.floor
+console.log(Math.floor(num1))
+
+// Arredondando o valor para cima com o método Math.ceil
+console.log(Math.ceil(num1))
+
+// Arredondando para o valor mais próximo com Math.round
+console.log(Math.round(num1))
+
+// Usando Math.max para pegar o maior número
+console.log(Math.max(1,2,4,5,6,10,55,55,22,44,71))
+
+// Usando Math.min para pegar o menor número
+console.log(Math.min(1,2,4,5,6,-0,-1,-5))
+
+// Usando Math.random para gerar um número aleatório entre 0 e 1 tirando o 1
+console.log(Math.random())
+
+// Usando Math.pow para potenciação de números
+console.log(Math.pow(2, 10))
