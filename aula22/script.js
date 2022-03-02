30e6fe415fdd7ad90abe7e36dd2373332e78efd6
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-02 16:32:36 -0300

    aula22

---
+++ aula22/script.js
@@ -0,0 +1,23 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> E //Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO 
+*/
+
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+
+console.log (!true) // Nega o true e inverte pra false
+console.log(!false) // Nega o false e inverte pra true
