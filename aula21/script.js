fea2cca86b6bd3d887bebd8ba200053a4332d4ce
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-02 15:36:50 -0300

    aula21

---
+++ aula21/script.js
@@ -0,0 +1,14 @@
+/*
+Operadores de comparação
+
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== Igualdade (Só checa valor)
+=== Igualdade estrita (Checa valor e tipo)
+!= Diferente (Só checa valor)
+!== Diferente estrito (Checa valor e tipo)
+*/
+
+console.log(10 !== '10')
