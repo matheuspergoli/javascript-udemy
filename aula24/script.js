acbd7ab9295cedbe383158adf01d968053f3262a
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-02 17:36:36 -0300

    aula24

---
+++ aula24/script.js
@@ -0,0 +1,31 @@
+/*
+Entre 0 - 11 = Bom dia
+Entre 12 - 17 = Boa tarde
+Entre 18 - 23 = Boa noite
+*/
+
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes
+// Eu posso ter vários else if na condição
+// Só posso ter um else na condição
+// Podemos usar condições sem else if, utilizando apenas if e else
+
+/*
+const hora = 10
+
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora < 17) {
+    console.log('Boa tarde!')
+} else if (hora < 23) {
+    console.log('Boa noite!')
+}
+*/
+
+const tenhoGrana = false
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa')
+} else {
+    console.log('Não vou sair de casa')
+}
