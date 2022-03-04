7587c844eef8ba79505934dbdd117256db9b37f7
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-03 22:51:44 -0300

    aula27

---
+++ aula27/script.js
@@ -0,0 +1,7 @@
+// Operador ternário
+// Condição ? 'Valor para verdadeiro' : 'Valor para falso'
+
+const pontos = 999
+const nivel = pontos >= 1000 ? 'Usuário VIP' : 'Usuário Normal'
+
+console.log(nivel)
