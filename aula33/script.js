f29dcfcdfee05119a038089c1fc109ddc8ca8e79
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-06 11:14:51 -0300

    aula33

--- aula33/script.js
+++ aula33/script.js
@@ -1,7 +1,7 @@
 const pessoa = {
-    // nome: 'Matheus',
-    // sobrenome: 'Pergoli',
-    // idade: 22,
+    nome: 'Matheus',
+    sobrenome: 'Pergoli',
+    idade: 22,
     endereco: {
         rua: 'São Marçal',
         bairro: 'Jardim Helena',
@@ -13,8 +13,12 @@ const pessoa = {
 // const { endereco: {rua, bairro, casa}} = pessoa
 // console.log(rua, bairro, casa)
 
-const {nome = 'Nome não definido', sobrenome = 'Sobrenome não difinido', idade = 'Idade não definida'} = pessoa
+// const {nome = 'Nome não definido', sobrenome = 'Sobrenome não difinido', idade = 'Idade não definida'} = pessoa
 
-console.log(nome)
-console.log(sobrenome)
-console.log(idade)
+// console.log(nome)
+// console.log(sobrenome)
+// console.log(idade)
+
+const {endereco: {rua: minhaRua, bairro: meuBairro, casa: minhaCasa}} = pessoa
+
+console.log(`Eu moro na rua ${minhaRua} no bairro ${meuBairro} do número ${minhaCasa}`)
