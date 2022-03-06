a3d9b161bf98e248fdc8fc2d6a352af243423490
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-06 17:33:12 -0300

    Create script.js

---
+++ aula34/script.js
@@ -0,0 +1,5 @@
+// i - index
+const nomes = ['Matheus', 'Karla', 'Sophia', 'Arthur', 'Luiz', 'Denise', 'Pedro']
+for (let i = 0; i < nomes.length; i++) {
+    console.log(`Índice ${i} contém ${nomes[i]}`)
+}
