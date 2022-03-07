0cb7b38efb662138a5335c2a1c75096f66478078
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-07 20:06:49 -0300

    aula41

---
+++ aula41/script.js
@@ -0,0 +1,7 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function maiorValor(valorUm, valorDois) {
+    return Math.max(valorUm, valorDois)
+}
+
+console.log(maiorValor(150, 250))
