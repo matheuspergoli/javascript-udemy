6b39a6ab32df296f3676748d88458c994919e1b3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-18 18:36:26 -0300

    aula75

---
+++ intermediario - objects/aula75/script.js
@@ -0,0 +1,32 @@
+function Produto(nome, preco) {
+    this.nome = nome
+    this.preco = preco
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100))
+}
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100))
+}
+
+const p1 = new Produto('Camiseta', 50)
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+}
+Object.setPrototypeOf(p2, Produto.prototype)
+
+p2.aumento(10)
+
+const p3 = Object.create(Produto.prototype, {
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42
+    }
+})
+console.log(p3)
